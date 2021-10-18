73aa42bc5d68b49150d9beaf958e2c27d29be985
Ezequiel Miccio <ezequielmiccio@gmail.com>
2021-10-18 00:33:07 -0300

    section hombre

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => { 
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle("nav-menu");
 });
+
+/* addEventListener("DOMContentLoaded", () => {
+    
+    const navToggle = document.querySelector(".nav-toggle")
+    if (nav-toggle) {
+        navToggle.addEventListener("click", () => { 
+            const navMenu = document.querySelector(".nav-menu");
+            navMenu.classList.toggle("show"); 
+        })
+        
+    }
+}); */
