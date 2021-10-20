f66a953a8265ede406a29cacc91a5ae48624bfb7
Ezequiel Miccio <ezequielmiccio@gmail.com>
2021-10-20 02:04:30 -0300

    hasta mas tarde

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => { 
-    navMenu.classList.toggle("nav-menu");
+    navMenu.classList.toggle("nav-menu_visible");
 });
 
 /* addEventListener("DOMContentLoaded", () => {
