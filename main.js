b4c03469919ef09faf6c69790dddc704b8a5660e
Ezeequiel Miccio <ezequielmiccio@gmail.com>
2021-10-29 15:50:57 -0300

    responsive men

--- main.js
+++ main.js
@@ -4,15 +4,3 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => { 
     navMenu.classList.toggle("nav-menu_visible");
 });
-
-/* addEventListener("DOMContentLoaded", () => {
-    
-    const navToggle = document.querySelector(".nav-toggle")
-    if (nav-toggle) {
-        navToggle.addEventListener("click", () => { 
-            const navMenu = document.querySelector(".nav-menu");
-            navMenu.classList.toggle("show"); 
-        })
-        
-    }
-}); */
